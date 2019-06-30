a36d0e07e3ded346a7e9ef09da0648c9b443175e
Yasser Alnader <yasseralnader@gmail.com>
2019-06-29 21:41:48 -0400

    created components folder, created SideBar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import SideBar from "./components/SideBar";
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <SideBar />
         </a>
       </header>
     </div>
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <div>
+      <span>WORKS!</span>
+    </div>
+  );
+}
+
+export default SideBar;
