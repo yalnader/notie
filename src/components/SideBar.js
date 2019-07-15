87126e007304ca00e35076a2ffc3579fd760ad4a
Yasser Alnader <yasseralnader@gmail.com>
2019-07-15 17:57:15 -0400

    begining of sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,25 @@
 import React from "react";
+// import FontAwesome from "font-awesome";
+import "font-awesome/css/font-awesome.min.css";
+var FontAwesome = require("react-fontawesome");
 
+var divStyle = {
+  color: "#f06595"
+};
 function SideBar() {
   return (
     <div>
-      <span>WORKS!</span>
+      <ul class="menu">
+        <li>
+          <FontAwesome name="home" size="3x" />
+        </li>
+        <li>
+          <FontAwesome name="gear" size="3x" />
+        </li>
+        <li>
+          <FontAwesome name="question" size="3x" />
+        </li>
+      </ul>
     </div>
   );
 }
