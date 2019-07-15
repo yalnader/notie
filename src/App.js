01e027ba2fd481afdd543d2ad3aa86f2b1923c69
Kelvin <kelvinh.work@hotmail.com>
2019-07-15 15:57:31 -0400

    Updated favicon. Added loading view.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from "react";
-import logo from "./logo.svg";
+import ReactDOM from "react-dom";
+import notielogo from "./pictures/notie.png";
+import Button from 'react-bootstrap/Button';
+
+import Circles from './components/Circles'
+import CONSTANTS from './constants/constants'
 import "./App.css";
 
-import SideBar from "./components/SideBar";
-
 function App() {
+  const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+  function startLoad(e){
+    ReactDOM.render( <Circles /> , document.getElementById('container'));
+    let loadButton = document.getElementById("startButton");
+    let i = 0;
+    loadButton.innerText = CONSTANTS.loadingEvents[i++];
+    sleep(2000).then(() => {
+      loadButton.innerText = CONSTANTS.loadingEvents[i++];
+      sleep(2000).then(() => {
+        loadButton.innerText = CONSTANTS.loadingEvents[i];
+        sleep(2000).then(() => {
+          // RENDER HOME VIEW HERE
+        })
+      })
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SideBar />
-        </a>
+      <header className="container">
+        <img src={notielogo} className="App-logo" alt="logo" />
+        <Button id="startButton" variant="warning" size="lg" onClick={startLoad}>
+          Start Jotting!
+        </Button>
+        <div id="container"></div>
       </header>
     </div>
   );
